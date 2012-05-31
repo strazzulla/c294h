0f98498d4c36d7ce312eedc9ffee6291c02199df
Daniel Strazzulla <strazzulla@gmail.com>
2012-05-31 14:46:05 -0700

    added parse's baseline Xcode project

---
+++ ParseStarterProject/ParseStarterProject/ParseStarterProjectViewController.h
@@ -0,0 +1,10 @@
+// Copyright 2011 Ping Labs, Inc. All rights reserved.
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface ParseStarterProjectViewController : UIViewController {
+    
+}
+
+@end
